ed6000bdf79c5d2d159f52468239d88ab9f25a79
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-02-23 18:47:12 +0530

    #commit after Adding Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/layout/Footer'
+import NavBar from './components/layout/NavBar'
+import Landing from './components/layout/Landing'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar/>
+        <Landing/>
+        <Footer/>
       </div>
+      </Router>
     );
   }
 }
