8e09dea1d078473b0b5647faf4d05be256fd90b7
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-03-02 01:04:28 +0530

    #fixed minor bugs

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/publicProfile',publicProfile);
 //Server static assets if in production
 if(process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
   app.get('*',(req,res) => {
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   })
