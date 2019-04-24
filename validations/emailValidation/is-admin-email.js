981fcf972ff4bf7f3a801dd87d06cf2ffb2884b2
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-04-24 21:06:06 +0530

    #FINAL PUSH FOR PROJECT SPRINT-3 #Added rohan,dheeraj, SE prof admin accounts

--- validations/emailValidation/is-admin-email.js
+++ validations/emailValidation/is-admin-email.js
@@ -1,5 +1,7 @@
 const isAdminEmail = value => {
-  return (value==='archeus@hyderabad.bits-pilani.ac.in') || (value==='darthCoffee@hyderabad.bits-pilani.ac.in') || (value==='baswath@hyderabad.bits-pilani.ac.in');
+  return (value==='archeus@hyderabad.bits-pilani.ac.in') || (value==='darthCoffee@hyderabad.bits-pilani.ac.in') ||
+    (value==='baswath@hyderabad.bits-pilani.ac.in') || (value==='rohan@hyderabad.bits-pilani.ac.in')
+    || (value==='dheeru@hyderabad.bits-pilani.ac.in') || (value==='seic@hyderabad.bits-pilani.ac.in');
 }
 
 module.exports = isAdminEmail;
